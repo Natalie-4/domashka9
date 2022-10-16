cd5d33264c011b1065fa9fe02ed4e40f08e1e210
Natalie <itlaym@yandex.ru>
2022-10-16 19:33:09 +0700

    solve task_66

--- task_66/Program.cs
+++ task_66/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120          M = 4; N = 8. -> 30           */
+
+
+
+int SummaNaturalNumbers(int M, int N)
+{
+    if (M == N)
+    return N;
+    return SummaNaturalNumbers(M + 1, N) + (N - M);
+}
+
+Console.WriteLine("Введите значение M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SummaNaturalNumbers(M,N));
